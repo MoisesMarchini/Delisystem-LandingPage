a2425f6fb816b30506076df3e050629490852d69
Moisés Marchini <moisesmarchinipereira@gmail.com>
2024-11-27 09:45:07 -0300

    feat: hover effect on pricingcard;

--- src/app/sections/pricing/pricing-card/pricing-card.component.ts
+++ src/app/sections/pricing/pricing-card/pricing-card.component.ts
@@ -1,13 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { PrimaryButtonComponent } from "../../../components/buttons/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from "../../../components/buttons/secondary-button/secondary-button.component";
 import { CommonModule } from '@angular/common';
 import { PricingItem } from '../../../models/items/pricing-item';
 
 @Component({
   selector: 'app-pricing-card',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent, CommonModule],
+  imports: [PrimaryButtonComponent, CommonModule],
   templateUrl: './pricing-card.component.html',
   styleUrl: './pricing-card.component.scss',
   host: {
@@ -16,8 +15,19 @@ import { PricingItem } from '../../../models/items/pricing-item';
 })
 export class PricingCardComponent {
   @Input() pricingItem: Partial<PricingItem> = {};
+  static currentHoverItem?: string;
+  get currentHoverItem() {
+    return PricingCardComponent.currentHoverItem;
+  }
 
-  onButtonClick() {
+  @HostListener('mouseenter')
+  onHover() {
+    PricingCardComponent.currentHoverItem = this.pricingItem.name;
+  }
 
+  @HostListener('mouseleave')
+  onBlur() {
+    if (PricingCardComponent.currentHoverItem === this.pricingItem.name)
+      PricingCardComponent.currentHoverItem = undefined;
   }
 }
